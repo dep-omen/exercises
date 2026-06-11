90a1fad1450dcfbfcbbafc4c5fb71e53e9c64587
Deptroit <deptroit1171@gmail.com>
2026-06-11 15:39:41 +0530

    exercise 3 used pointers

--- exercise03_functions.c
+++ exercise03_functions.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 #include <stdint.h>
-int multiply(int num1, int num2){
+int32_t multiply(int *pn1, int *pn2){
 	
-	int mul;
-	mul = num1 * num2;
+	int32_t mul;
+	mul = *pn1 * *pn2;
+	
 	return mul;
 
 }
 
 int main(){
 	
-	int result;
-	printf("multiplication is = %d\n",result=multiply(5,5));
+	int32_t result;
+	int32_t num1,num2;
+	num1 = 5;
+	num2 = 5;
+	result = multiply(&num1,&num2);
+	printf("number 1 = %d, number 2 = %d \nmultiplication is = %d\n",num1,num2,result);
 	return 0;
 
 }

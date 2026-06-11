a6aaf6a11f90ad179828bed092199bb47a53f817
Deptroit <deptroit1171@gmail.com>
2026-06-11 15:04:20 +0530

    Exercise 3 on the functions functions uses parameters and it is multiplication function

---
+++ exercise03_functions.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdint.h>
+int multiply(int num1, int num2){
+	
+	int mul;
+	mul = num1 * num2;
+	return mul;
+
+}
+
+int main(){
+	
+	int result;
+	printf("multiplication is = %d\n",result=multiply(5,5));
+	return 0;
+
+}

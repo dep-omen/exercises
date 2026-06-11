89b2f2276d739f8a212bc4428d34b312e338b59b
Deptroit <deptroit1171@gmail.com>
2026-06-11 15:58:04 +0530

    Exercise 3 done

--- exercise03_functions.c
+++ exercise03_functions.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdint.h>
-int32_t multiply(int *pn1, int *pn2){
+int32_t multiply(int32_t n1, int32_t n2){
 	
 	int32_t mul;
-	mul = *pn1 * *pn2;
+	mul = n1 * n2;
 	
 	return mul;
 
@@ -13,10 +13,34 @@ int main(){
 	
 	int32_t result;
 	int32_t num1,num2;
-	num1 = 5;
-	num2 = 5;
-	result = multiply(&num1,&num2);
-	printf("number 1 = %d, number 2 = %d \nmultiplication is = %d\n",num1,num2,result);
+	int32_t n = 0;
+
+	printf("How many times you want to perform multiplication --> ");
+		if(scanf("%d", &n) != 1){
+	 	        printf("Invalid input\n");
+    			return 1;
+		}
+
+	for(int32_t i=1;i<=n;i++){
+
+		printf("%d.\n",i);
+		printf("Enter your first number  --> ");
+		    if(scanf("%d", &num1) != 1){
+                        printf("Invalid input\n");
+                        return 1;
+                }
+
+		printf("Enter your second number --> ");
+		    if(scanf("%d", &num2) != 1){
+                        printf("Invalid input\n");
+                        return 1;
+                }
+
+		result = multiply(num1,num2);
+		printf("number 1 = %d, number 2 = %d \nmultiplication is = %d\n",num1,num2,result);
+
+	}
+
 	return 0;
 
 }

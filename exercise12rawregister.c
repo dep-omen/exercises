57e8a0ee77840da0085c6b0f11260aa46a3b7725
Deptroit <deptroit1171@gmail.com>
2026-06-16 18:28:29 +0530

    Exercise 12 done based on memory mapping important one

--- exercise12rawregister.c
+++ exercise12rawregister.c
@@ -47,14 +47,65 @@
 #include <sys/mman.h>
 #include <unistd.h>
 
+void delay(){
+    
+    for(volatile uint32_t i = 0; i < 100000000; i++){
+        //wait
+    }
+
+}
+
 int main(){
 
     int fd = open("/dev/mem", O_RDWR | O_SYNC);
-    if (fd!=-1){
+        if (fd==-1){
+            printf("Cannot access memory\n");
+            return 1;
+        }
         printf("Memory accessed\n");
-    }
-    else
-         printf("Cannot access memory\n");
-    return 0;
+    void *gpioh_map = mmap(NULL, 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0x50009000);
+        if(gpioh_map==MAP_FAILED){
+            printf("Failed to MMAP memory\n");
+            return 1;
+        }
+        printf("Memory Mapped for GPIOH\n");
 
+    volatile uint32_t *gpioh_bsrr = (volatile uint32_t *)((uint8_t *)gpioh_map + 0x18);
+    volatile uint32_t *gpioh_moder = (volatile uint32_t *)((uint8_t *)gpioh_map + 0x00);
+
+    void *rcc_map = mmap(NULL, 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0x50000000);
+        if(rcc_map==MAP_FAILED){
+            printf("Failed to MMAP memory\n");
+            return 1;
+        }
+        printf("Memory Mapped for RCC\n");
+
+    volatile uint32_t *RCC_AHB4ENSETR = (volatile uint32_t *)((uint8_t *)rcc_map + 0xA28);
+        
+        *RCC_AHB4ENSETR |= (1<<7);
+        *gpioh_moder |= (1<<14); 
+        *gpioh_moder &=~ (1<<15);
+
+        while(1){
+
+        for(uint32_t i = 0; i<5; i++){
+
+            *gpioh_bsrr = (1<<7);
+            printf("LED ON\n");
+            
+            delay();
+
+            *gpioh_bsrr = (1<<23);
+            printf("LED OFF\n");
+            delay();
+
+        }
+    }
+    munmap(gpioh_map, 0x1000);
+    munmap(rcc_map, 0x1000);
+    close(fd);
+    return 0;
 }
+
+
+
